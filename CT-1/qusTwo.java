717922f992d84b98fdf0ffb2982906a4311c1547
Mahmudul Hasan Shovon <101075577+im-mahmudul@users.noreply.github.com>
2024-11-30 13:03:44 +0600

    Add files via upload

---
+++ CT-1/qusTwo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class qusTwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        System.out.println(line);
+
+        for(Strin = a ){
+
+        }
+
+    }
+}
